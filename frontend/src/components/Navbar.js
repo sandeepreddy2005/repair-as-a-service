7e17ac02b4eaa73f1394111855a41b5a781754f4
sandeepreddy2005 <your-github-22r01a0451@cmrithyderabad.edu.in>
2026-01-06 22:38:53 +0530

    Remove signup/login links from navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,35 +2,10 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={styles.nav}>
-      <h3 style={styles.logo}>Repair-as-a-Service</h3>
-      <div>
-        <Link to="/" style={styles.link}>Home</Link>
-        <Link to="/login" style={styles.link}>Login</Link>
-        <Link to="/signup" style={styles.link}>Signup</Link>
-      </div>
+    <nav>
+      <Link to="/request">Request a Repair</Link>
     </nav>
   );
 }
-
-const styles = {
-  nav: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "15px 30px",
-    backgroundColor: "#2563eb",
-  },
-  logo: {
-    color: "white",
-    margin: 0,
-  },
-  link: {
-    color: "white",
-    marginLeft: "20px",
-    textDecoration: "none",
-    fontWeight: "bold",
-  },
-};
 
 export default Navbar;
